8d7e094b31d01b706de4c5a041315e7d77e7ed51
andrew_young1808 <11034301@uvu.edu>
2024-10-03 13:31:59 -0600

    Lab 3: Challenge

---
+++ AdventureGame/Interactions and Collisions new/Assets/scripts/ColorChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Color newColor = Color.red;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Renderer>().material.color = newColor;
+    }
+}
